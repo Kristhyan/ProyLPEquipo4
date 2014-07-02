c0a0a0ef82a3d09dcc0b1c2c8513870d43c4ba4a
alexiis <ale.jin2009@hotmail.com>
2014-07-02 18:12:25 -0500

    Actividad
    En estas lineas desarrollamos el menu con las variables asignadas
    anteriormente

--- librerias.cpp
+++ librerias.cpp
@@ -8,5 +8,18 @@ char descripcion[300];
 struct tm fechainic, fechafina;
 main ()
 {
+Evento mi Evento;
+cout<<"Ingrese el nombre del evento";
+   cin>>mievento.nombrecorto;
+   cout<<"Ingrese la descripcion del evento";
+   cin>>mievento.descripcion;
+   cout<<"Ingrese la fecha(dd/mm/aaaa)";
+   cin>>mievento.fechainic.tm_mday;
+   cin>>mievento.fechainic.tm_mon;
+   cin>>mievento.fechainic.tm_year;
+    cout<<"Ingrese la fecha(dd/mm/aaaa)";
+    cin>>mievento.fechafina.tm_mday;
+    cin>>mievento.fechafina.tm_mon;
+    cin>>mievento.fechafina.tm_year;
 	getch();
 }
